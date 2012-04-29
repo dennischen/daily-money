fb0303bd30191a3724d524c1970df22edb73eaa3
LancelotLiu <lancelot.yl.liu@gmail.com>
2012-04-30 05:33:07 +0800

    prevent backup job from executing on app starting.

--- dailymoney/src/com/bottleworks/dailymoney/data/ScheduleJob.java
+++ dailymoney/src/com/bottleworks/dailymoney/data/ScheduleJob.java
@@ -9,9 +9,10 @@ public class ScheduleJob {
     public Calendar getInitDate() {
         if(initDate == null) {
             initDate = Calendar.getInstance();
-            initDate.set(Calendar.YEAR, 2011);
-            initDate.set(Calendar.MONTH, Calendar.JANUARY);
-            initDate.set(Calendar.DAY_OF_MONTH, 3);
+            int currentHour = initDate.get(Calendar.HOUR_OF_DAY);
+            if(currentHour > 3) {
+                initDate.add(Calendar.DATE, 1);
+            }
             initDate.set(Calendar.HOUR_OF_DAY, 3);
             initDate.set(Calendar.MINUTE, 0);
             initDate.set(Calendar.SECOND, 0);
